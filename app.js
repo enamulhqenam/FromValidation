ae79bb2af495802f580e97b11beb30cb4540263c
Enamulhqenam <enamulhqenam50607210@gmail.com>
2024-04-29 13:22:15 +0000

    comment line

---
+++ app.js
@@ -0,0 +1,22 @@
+const signinBtn = document.querySelector("#signin-btn");
+const signUpBtn = document.querySelector("#signup-btn");
+const container = document.querySelector(".container");
+
+const signinBtn2 = document.querySelector("#signin-btn2");
+const signUpBtn2 = document.querySelector("#signup-btn2");
+
+
+signUpBtn.addEventListener("click",() =>{
+	container.classList.add("signUp-mode")
+});
+
+signinBtn.addEventListener("click",() =>{
+	container.classList.remove("signUp-mode")
+});
+
+// signUpBtn2.addEventListener("click",()=>{
+// 	container.classList.add("SignUp-mode")
+// });
+signinBtn2.addEventListener("click",() =>{
+	container.classList.remove("signUp-mode2")
+});
